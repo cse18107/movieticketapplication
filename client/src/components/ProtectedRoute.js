a49b6563207ac461dcc2d742352bc9ead31a34e7
cse18107 <cse18107@cemk.ac.in>
2023-02-27 23:21:30 +0530

    added home page layout

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -39,8 +39,29 @@ const ProtectedRoute = ({children}) => {
     }, [])
     console.log(user)
   return (
-    user && (<div>
-        {children}
+    user && (<div className="layout p-1">
+        <div className='header bg-primary flex justify-between p-2'>
+            <div>
+                <h1 className='text-2x1 text-white'>
+                    BOOKMYMOVIE
+                </h1>
+            </div>
+            <div className='bg-white p-1 flex gap-1'>
+                < i className = "ri-shield-user-line text-primary" > </i>
+                <h1 className='text-sm '>
+                    {user.name}
+                </h1>
+                < i className = "ri-logout-box-r-line ml-2"
+                onClick={()=> {
+                    localStorage.removeItem("token");
+                    navigate("/login")
+                }}
+                > </i>
+            </div>
+        </div>
+        <div className='content mt-1 p-1'>
+            {children}
+        </div>  
     </div>)
   )
 }
