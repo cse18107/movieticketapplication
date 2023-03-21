f121a74d08071dccb52d46c3db96847948259836
cse18107 <cse18107@cemk.ac.in>
2023-03-21 13:00:46 +0530

    added shows in theatre

--- client/src/pages/Profile/TheatersList.js
+++ client/src/pages/Profile/TheatersList.js
@@ -6,6 +6,7 @@ import { DeleteTheatre, GetAllTheatresByOwner } from "../../apicalls/theatres";
 import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { Table, message } from "antd";
+import Shows from "./Shows";
 
 const TheatersList = () => {
   const { user } = useSelector((state) => state.users);
@@ -13,6 +14,8 @@ const TheatersList = () => {
   const [selectedTheater, setSelectedTheater] = useState(null);
   const [formType, setFormType] = useState("add");
   const [theaters, setTheaters] = useState([]);
+
+  const [openShowsModal = false, setOpenShowsModal] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -69,12 +72,12 @@ const TheatersList = () => {
       title: "Status",
       dataIndex: "isActive",
       render: (text, record) => {
-        if(text) {
-          return 'Approved'
+        if (text) {
+          return "Approved";
         } else {
-          return 'Pending / Blocked'
+          return "Pending / Blocked";
         }
-      }
+      },
     },
     {
       title: "Action",
@@ -96,7 +99,17 @@ const TheatersList = () => {
                 setShowTheaterFormModal(true);
               }}
             ></i>
-            {record.isActive && <span className="underline">Shows</span>}
+            {record.isActive && (
+              <span
+                className="underline"
+                onClick={() => {
+                  setSelectedTheater(record);
+                  setOpenShowsModal(true);
+                }}
+              >
+                Shows
+              </span>
+            )}
           </div>
         );
       },
@@ -131,6 +144,13 @@ const TheatersList = () => {
           selectedTheater={selectedTheater}
           setSelectedTheater={setSelectedTheater}
           getData={getData}
+        />
+      )}
+      {openShowsModal && (
+        <Shows
+          openShowsModal={openShowsModal}
+          setOpenShowsModal={setOpenShowsModal}
+          theatre={selectedTheater}
         />
       )}
     </div>
